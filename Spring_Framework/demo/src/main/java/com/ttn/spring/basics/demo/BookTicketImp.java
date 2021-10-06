98535930c84b3461038b5ceb34b437c2b84f18db
GuptaVaishali <vaishaligupta982@gmail.com>
2021-10-06 13:17:56 +0530

    Added Component and autowired annotation for dependency management

--- Spring_Framework/demo/src/main/java/com/ttn/spring/basics/demo/BookTicketImp.java
+++ Spring_Framework/demo/src/main/java/com/ttn/spring/basics/demo/BookTicketImp.java
@@ -1,8 +1,13 @@
 package com.ttn.spring.basics.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BookTicketImp {
     TravelMedium travelMedium;
 
+    @Autowired
     BookTicketImp(TravelMedium travelMedium){
         this.travelMedium = travelMedium;
     }
--- Spring_Framework/demo/src/main/java/com/ttn/spring/basics/demo/DemoApplication.java
+++ Spring_Framework/demo/src/main/java/com/ttn/spring/basics/demo/DemoApplication.java
@@ -2,21 +2,23 @@ package com.ttn.spring.basics.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		//---Tight Coupling------
+		//--- Tight Coupling ------
 		//	BookTicketImp bookTicketImp = new BookTicketImp();
 
-		//Loose Coupling
-		BookTicketImp bookTicketImp = new BookTicketImp(new AirTicket());
+		//------- Loose Coupling --------
+		//BookTicketImp bookTicketImp = new BookTicketImp(new AirTicket());
+
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		BookTicketImp bookTicketImp = applicationContext.getBean(BookTicketImp.class);
 		String source_dest = bookTicketImp.bookTicket("Delhi","Goa");
 		System.out.println(source_dest);
-
-		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
